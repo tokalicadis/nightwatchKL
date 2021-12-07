ba56b9aea932609638891a0256fdcebce220f6cc
tokalicadis <35777973+tokalicadis@users.noreply.github.com>
2021-12-07 13:54:39 +0100

    Added DeleteAgency.js and updated its functionality

---
+++ untest/AgencyForm/DeleteAgency.js
@@ -0,0 +1,44 @@
+//BRIŠI AGENCIJU
+
+
+module.exports = {
+    'Demo test kl-test' : function(browser) {
+        browser
+            //DRIVER SETUP
+            .url('https://test.kl.hexis.dev/')
+            .maximizeWindow()
+            .waitForElementVisible('body')
+            .useCss()
+            .click(".btn")
+            .useXpath() // every selector now must be xpath
+
+            //LOGIN USERNAME
+            .waitForElementVisible("//*[@id =\'identifierId\']")
+            .setValue("//*[@id =\'identifierId\']", 'adis@hexis.hr')
+            .click("//*[@id ='identifierNext']")
+
+            //LOGIN PASSWORD
+            .waitForElementVisible("//*[@id ='password']/div[1]/div / div[1]/input")
+            .setValue("//*[@id ='password']/div[1]/div / div[1]/input", 'chillstone54')
+            .click("//*[@id ='passwordNext']")
+
+            //AGENCIJE, NAVIGACIJA
+            .useCss() // we're back to CSS now
+            .waitForElementVisible("#BusinessDropdown")
+            .click("#BusinessDropdown")
+            .click('link text', 'Agencije')
+
+            ///BRIŠI AGENCIJU FORMA
+            .click('#dataTables-example tr:nth-child(1) > td:nth-child(1)')
+            .waitForElementVisible(".btn-danger")
+            .click('.btn-danger')
+            .pause(2000)
+            .acceptAlert()
+            .pause(2000)
+
+
+
+            .end();
+
+    }
+};
