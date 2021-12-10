f8a9af4fec63be756e41619603269df9105f3bb9
tokalicadis <35777973+tokalicadis@users.noreply.github.com>
2021-12-10 09:44:22 +0100

    Added DeleteShipForm.js and updated its functionality

---
+++ untest/ShipForm/DeleteShipForm.js
@@ -0,0 +1,45 @@
+//IZBRIŠI BROD
+
+
+module.exports = {
+    'Demo test kl-test' : function(browser) {
+        browser
+            //DRIVER SETUP
+            .url('https://test.kl.hexis.dev/')
+            .maximizeWindow()
+            .waitForElementVisible('body')
+            .useCss()
+            .click(".btn")
+            .useXpath() // every selector now must be xpath
+
+            //LOGIN USERNAME
+            .waitForElementVisible("//*[@id =\'identifierId\']")
+            .setValue("//*[@id =\'identifierId\']", 'adis@hexis.hr')
+            .click("//*[@id ='identifierNext']")
+
+            //LOGIN PASSWORD
+            .waitForElementVisible("//*[@id ='password']/div[1]/div / div[1]/input")
+            .setValue("//*[@id ='password']/div[1]/div / div[1]/input", 'chillstone54')
+            .click("//*[@id ='passwordNext']")
+
+            //BROD NAVIGACIJA
+            .useCss() // we're back to CSS now
+            .waitForElementVisible("#ShipsDropdown")
+            .click("#ShipsDropdown")
+            .click('link text', 'Brodovi')
+
+            //BRIŠI BROD
+            .click('#dataTables-example tr:nth-child(1) > td:nth-child(1)')
+            .waitForElementVisible(".btn-danger")
+            .click('.btn-danger')
+
+            .pause(2000)
+            .acceptAlert()
+            .pause(5000)
+
+
+
+            .end();
+
+    }
+};
