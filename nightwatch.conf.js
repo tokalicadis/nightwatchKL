a8fc92d80904179e3234c437c53c8d28e4a05c8c
tokalicadis <35777973+tokalicadis@users.noreply.github.com>
2021-12-16 09:10:16 +0100

    Modified conf file for WebStorm

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,7 +5,7 @@ const Services = {}; loadServices();
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ["C:/Users/Korisnik/IdeaProjects/nightwatchKLGit/tests"],
+  src_folders: ["C:/Users/Korisnik/WebstormProjects/nightwatchKL/tests"],
 
   // See https://nightwatchjs.org/guide/working-with-page-objects/
   page_objects_path: '',
